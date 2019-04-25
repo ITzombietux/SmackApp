86ca451cdcf82b0db0d76800d077d6f0ecae565b
zombietux <zombietux@Macui-MacBook-Pro-2.local>
2019-04-25 14:36:14 +0900

    Setting SWReveal.

---
+++ SmackApp/Controllers/ChannelViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ChannelViewController.swift
+//  SmackApp
+//
+//  Created by zombietux on 25/04/2019.
+//  Copyright © 2019 zombietux. All rights reserved.
+//
+
+import UIKit
+
+class ChannelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.revealViewController()?.rearViewRevealWidth = self.view.frame.size.width - 60
+    }
+    
+
+
+}
---
+++ SmackApp/Controllers/ChatViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ChatViewController.swift
+//  SmackApp
+//
+//  Created by zombietux on 25/04/2019.
+//  Copyright © 2019 zombietux. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    @IBOutlet weak var menuButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        menuButton.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+    
+    
+}
