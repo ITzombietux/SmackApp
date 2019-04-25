011585ada2f38283206e11d103bdd92a14de634d
zombietux <zombietux@Macui-MacBook-Pro-2.local>
2019-04-25 18:47:34 +0900

    CreateAccountViewController UI

---
+++ SmackApp/Controllers/CreateAccountViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CreateAccountViewController.swift
+//  SmackApp
+//
+//  Created by zombietux on 25/04/2019.
+//  Copyright © 2019 zombietux. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        performSegue(withIdentifier: UNWIND, sender: nil)
+    }
+
+}
