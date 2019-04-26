11155a820fd79b05f0a2c4ce79e300c13cbe86ef
zombietux <zombietux@Macui-MacBook-Pro-2.local>
2019-04-26 10:54:11 +0900

    Register a user

--- SmackApp/Controllers/CreateAccountViewController.swift
+++ SmackApp/Controllers/CreateAccountViewController.swift
@@ -9,11 +9,36 @@
 import UIKit
 
 class CreateAccountViewController: UIViewController {
-
+    
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var userImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
+    }
+    
+    
+    @IBAction func createAccountPressed(_ sender: Any) {
+        guard let email = emailText.text , emailText.text != "" else { return }
+        guard let password = passwordText.text, passwordText.text != "" else { return }
+        
+        AuthService.instance.registerUser(email: email, password: password) { (success) in
+            
+            if success {
+                print("registered user!")
+            }
+        }
+    }
+    
+    @IBAction func pichAvatarPressed(_ sender: Any) {
+    }
+    
+    @IBAction func pickBGColorPressed(_ sender: Any) {
     }
     
     @IBAction func closePressed(_ sender: Any) {
