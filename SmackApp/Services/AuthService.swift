ebbd6a858e185331e7ec0bd635438fb14cfbb68a
zombietux <zombietux@Macui-MacBook-Pro-2.local>
2019-04-26 13:58:27 +0900

    Logging in a user

--- SmackApp/Services/AuthService.swift
+++ SmackApp/Services/AuthService.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class AuthService {
     
@@ -29,7 +30,7 @@ class AuthService {
             return defaults.value(forKey: TOKEN_KEY) as! String
         }
         set {
-            defaults.set(newValue, forKey:   TOKEN_KEY)
+            defaults.set(newValue, forKey: TOKEN_KEY)
         }
     }
     
@@ -46,15 +47,12 @@ class AuthService {
         
         let lowerCaseEmail = email.lowercased()
         
-        let header = [
-            "Content-Type": "application/json; charset=utf-8"
-        ]
         let body: [String: Any] = [
             "email": lowerCaseEmail,
             "password": password
         ]
         
-        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseString { (response) in
             
             if response.result.error == nil {
                 completion(true)
@@ -62,6 +60,35 @@ class AuthService {
                 completion(false)
                 debugPrint(response.result.error as Any)
             }
+        }
+    }
+    
+    func loginUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        
+        let lowerCaseEmail = email.lowercased()
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        
+        Alamofire.request(URL_LOGIN, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseJSON { (response) in
+            
+            if response.result.error == nil {
+
+            // Using SwiftyJSON
+            guard let data = response.data else { return }
+            let json = JSON(data: data)
+            self.userEmail = json["user"].stringValue
+            self.authToken = json["token"].stringValue
+            
+                self.isLoggedIn = true
+                completion(true)
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+           
         }
     }
 }
