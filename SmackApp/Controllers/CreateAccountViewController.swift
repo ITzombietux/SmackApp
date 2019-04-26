87e55dc66eee86bd56cdf8cb0fc55080a5ad77c8
zombietux <zombietux@Macui-MacBook-Pro-2.local>
2019-04-26 14:29:52 +0900

    Creating a User

--- SmackApp/Controllers/CreateAccountViewController.swift
+++ SmackApp/Controllers/CreateAccountViewController.swift
@@ -15,6 +15,9 @@ class CreateAccountViewController: UIViewController {
     @IBOutlet weak var passwordText: UITextField!
     @IBOutlet weak var userImage: UIImageView!
     
+    // Variables
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +27,7 @@ class CreateAccountViewController: UIViewController {
     
     
     @IBAction func createAccountPressed(_ sender: Any) {
+        guard let name = usernameText.text,  usernameText.text != "" else { return }
         guard let email = emailText.text , emailText.text != "" else { return }
         guard let password = passwordText.text, passwordText.text != "" else { return }
         
@@ -33,7 +37,13 @@ class CreateAccountViewController: UIViewController {
                 AuthService.instance.loginUser(email: email, password: password, completion: { (success) in
                     
                     if success {
-                        print("logged in user!", AuthService.instance.authToken)
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            
+                            if success {
+                                print(UserDataService.instance.name, UserDataService.instance.avatarName)
+                                self.performSegue(withIdentifier: UNWIND, sender: nil)
+                            }
+                        })
                     }
                 })
             }
